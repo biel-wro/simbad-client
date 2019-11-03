39d4028ba1bf3774a0231405a455bba58c2e7f54
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-11-03 21:38:41 +0100

    feat: remove timeout from notifications. fix notification styles

--- projects/simbad-client/src/app/core/notifications/notification.service.ts
+++ projects/simbad-client/src/app/core/notifications/notification.service.ts
@@ -9,35 +9,30 @@ export class NotificationService {
 
     default(message: string) {
         this.show(message, {
-            duration: 2000,
             panelClass: 'default-notification-overlay'
         });
     }
 
     info(message: string) {
         this.show(message, {
-            duration: 2000,
             panelClass: 'info-notification-overlay'
         });
     }
 
     success(message: string) {
         this.show(message, {
-            duration: 2000,
             panelClass: 'success-notification-overlay'
         });
     }
 
     warn(message: string) {
         this.show(message, {
-            duration: 2500,
             panelClass: 'warning-notification-overlay'
         });
     }
 
     error(message: string) {
         this.show(message, {
-            duration: 3000,
             panelClass: 'error-notification-overlay'
         });
     }
@@ -45,6 +40,6 @@ export class NotificationService {
     private show(message: string, configuration: MatSnackBarConfig) {
         // Need to open snackBar from Angular zone to prevent issues with its position per
         // https://stackoverflow.com/questions/50101912/snackbar-position-wrong-when-use-errorhandler-in-angular-5-and-material
-        this.zone.run(() => this.snackBar.open(message, null, configuration));
+        this.zone.run(() => this.snackBar.open(message, 'OK', configuration));
     }
 }
