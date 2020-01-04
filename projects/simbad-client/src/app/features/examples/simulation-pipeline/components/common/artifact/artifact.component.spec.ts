ff1fadfb9b813b0ed0ddee343ed7aa11fb4ada10
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-01-04 16:30:28 +0100

    test: fix mocks for artifact-component

--- projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/artifact/artifact.component.spec.ts
+++ projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/artifact/artifact.component.spec.ts
@@ -1,25 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArtifactComponent } from './artifact.component';
+import { MockComponent } from 'ng-mocks';
+import { InfoListComponent } from '@simbad-client/app/features/examples/simulation-pipeline/components/common/info-list/info-list.component';
 
 describe('ArtifactComponent', () => {
-  let component: ArtifactComponent;
-  let fixture: ComponentFixture<ArtifactComponent>;
+    let component: ArtifactComponent;
+    let fixture: ComponentFixture<ArtifactComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ArtifactComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        return TestBed.configureTestingModule({
+            declarations: [ArtifactComponent, MockComponent(InfoListComponent)]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ArtifactComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ArtifactComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
