b2e1609b2bada06c0a8b211397d8ef05f0e84b34
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-01-04 16:36:25 +0100

    test: fix mocks for artifacs-service

--- projects/simbad-client/src/app/features/examples/simulation-pipeline/core/services/artifacts-service.service.spec.ts
+++ projects/simbad-client/src/app/features/examples/simulation-pipeline/core/services/artifacts-service.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ArtifactsActionsService } from './artifacts-actions.service';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('ArtifactsActionsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [provideMockStore({ initialState: {} })]
+    }));
 
-  it('should be created', () => {
-    const service: ArtifactsActionsService = TestBed.get(ArtifactsActionsService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: ArtifactsActionsService = TestBed.get(ArtifactsActionsService);
+        expect(service).toBeTruthy();
+    });
 });
