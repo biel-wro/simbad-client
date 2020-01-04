34f1adb865c8ced7d0c57fc831d688e873406a89
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-01-04 16:37:28 +0100

    test: fix mocks for common pipeline components

--- projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/performance-monitor/performance-monitor.component.spec.ts
+++ projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/performance-monitor/performance-monitor.component.spec.ts
@@ -1,25 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PerformanceMonitorComponent } from './performance-monitor.component';
+import { MockComponent } from 'ng-mocks';
+import { StatusTileComponent } from '@simbad-client/app/features/examples/simulation-pipeline/components/common/status-tile/status-tile.component';
+import { MatDividerModule } from '@angular/material';
 
 describe('PerformanceMonitorComponent', () => {
-  let component: PerformanceMonitorComponent;
-  let fixture: ComponentFixture<PerformanceMonitorComponent>;
+    let component: PerformanceMonitorComponent;
+    let fixture: ComponentFixture<PerformanceMonitorComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PerformanceMonitorComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        return TestBed.configureTestingModule({
+            declarations: [PerformanceMonitorComponent, MockComponent(StatusTileComponent)],
+            imports: [MatDividerModule]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PerformanceMonitorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PerformanceMonitorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
