f5ee5a99037c21fa7e9ef8314077e9b8d9168371
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-01-04 16:52:15 +0100

    test: fix mocks for cli-step-component

--- projects/simbad-client/src/app/features/examples/simulation-pipeline/components/steps/cli-step/cli-step.component.spec.ts
+++ projects/simbad-client/src/app/features/examples/simulation-pipeline/components/steps/cli-step/cli-step.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CliStepComponent } from './cli-step.component';
+import { MockComponent } from 'ng-mocks';
+import { PerformanceMonitorComponent } from '@simbad-client/app/features/examples/simulation-pipeline/components/common/performance-monitor/performance-monitor.component';
+import { TaskContextComponent } from '@simbad-client/app/features/examples/simulation-pipeline/components/common/task-context/task-context.component';
+import { ArtifactListComponent } from '@simbad-client/app/features/examples/simulation-pipeline/components/common/artifact-list/artifact-list.component';
+import { SharedModule } from '@simbad-client/app/shared/shared.module';
+import { MatProgressBarModule } from '@angular/material';
+import { provideMockStore } from '@ngrx/store/testing';
 
-describe('ReportStepComponent', () => {
-  let component: CliStepComponent;
-  let fixture: ComponentFixture<CliStepComponent>;
+describe('CliStepComponent', () => {
+    let component: CliStepComponent;
+    let fixture: ComponentFixture<CliStepComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CliStepComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        return TestBed.configureTestingModule({
+            declarations: [
+                CliStepComponent,
+                MockComponent(PerformanceMonitorComponent),
+                MockComponent(TaskContextComponent),
+                MockComponent(ArtifactListComponent)
+            ],
+            imports: [SharedModule, MatProgressBarModule],
+            providers: [provideMockStore({ initialState: {} })]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CliStepComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CliStepComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
