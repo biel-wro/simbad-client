fa33e9cacdd700f0b7b4d5fd45ef2d8b7beb55cd
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-01-06 18:42:45 +0100

    feat: rewrite artifact-list component to use mat-table
    - custom table-like component was dropped, angular material table was
      added, with additional features such as pagination, searching artifact
      by name or sorting artifacts by their sizes or file types
    
    - warning: the sorting will break if the table is wrapped in *ngIf
      https://stackoverflow.com/questions/46893164/mat-table-sorting-demo-not-working
      I lost like 30 min. on this issue so beware

--- projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/artifact-list/artifact-list.component.spec.ts
+++ projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/artifact-list/artifact-list.component.spec.ts
@@ -1,9 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArtifactListComponent } from './artifact-list.component';
-import { MatDividerModule } from '@angular/material';
-import { MockComponent } from 'ng-mocks';
-import { InfoListComponent } from '@simbad-client/app/features/examples/simulation-pipeline/components/common/info-list/info-list.component';
+import {
+    MatDividerModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatTooltipModule
+} from '@angular/material';
+import { MockComponent, MockPipe } from 'ng-mocks';
+import { SmallInputActionComponent } from '@simbad-client/app/shared/small-input-action/small-input-action.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { IsPreviewEnabledPipe } from '@simbad-client/app/features/examples/simulation-pipeline/core/pipes/is-preview-enabled.pipe';
 
 describe('ArtifactListComponent', () => {
     let component: ArtifactListComponent;
@@ -11,8 +20,20 @@ describe('ArtifactListComponent', () => {
 
     beforeEach(async(() => {
         return TestBed.configureTestingModule({
-            declarations: [ArtifactListComponent, MockComponent(InfoListComponent)],
-            imports: [MatDividerModule]
+            declarations: [
+                ArtifactListComponent,
+                MockComponent(SmallInputActionComponent),
+                MockPipe(IsPreviewEnabledPipe)
+            ],
+            imports: [
+                MatDividerModule,
+                MatTableModule,
+                MatSortModule,
+                MatInputModule,
+                MatPaginatorModule,
+                MatTooltipModule,
+                TranslateModule.forRoot()
+            ]
         }).compileComponents();
     }));
 
--- projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/artifact-list/artifact-list.component.ts
+++ projects/simbad-client/src/app/features/examples/simulation-pipeline/components/common/artifact-list/artifact-list.component.ts
@@ -1,11 +1,54 @@
-import { Component, Input } from '@angular/core';
-import { ListElement } from '../info-list/info-list.component';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { ArtifactInfo } from '@simbad-cli-api/gen/models/artifact-info';
+import { MatPaginator, MatSort, MatTable, MatTableDataSource } from '@angular/material';
+import { Store } from '@ngrx/store';
+import {
+    downloadArtifact,
+    previewArtifact
+} from '@simbad-client/app/features/examples/simulation-pipeline/core/store/artifacts/artifacts.actions';
 
 @Component({
     selector: 'simbad-client-artifact-list',
     templateUrl: './artifact-list.component.html',
     styleUrls: ['./artifact-list.component.scss']
 })
-export class ArtifactListComponent {
-    @Input() artifacts: ListElement[];
+export class ArtifactListComponent implements OnChanges, OnInit {
+    @Input() data: ArtifactInfo[];
+
+    displayedColumns: string[] = ['name', 'fileType', 'sizeKb', 'actions'];
+    public dataSource = new MatTableDataSource<ArtifactInfo>();
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+    @ViewChild(MatTable) table: MatTable<any>;
+
+    constructor(private store: Store<{}>) {
+    }
+
+    ngOnInit() {
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.data) {
+            this.dataSource.data = changes.data.currentValue as ArtifactInfo[];
+        }
+    }
+
+    applyFilter(filterValue: string): void {
+        filterValue = filterValue.trim();
+        filterValue = filterValue.toLocaleLowerCase();
+        this.dataSource.filter = filterValue;
+    }
+
+    download(artifact: ArtifactInfo) {
+        const { id, name } = artifact;
+        this.store.dispatch(downloadArtifact({ id, name }));
+    }
+
+    preview(artifact: ArtifactInfo) {
+        const { id, name } = artifact;
+        this.store.dispatch(previewArtifact({ id, name }));
+    }
 }
