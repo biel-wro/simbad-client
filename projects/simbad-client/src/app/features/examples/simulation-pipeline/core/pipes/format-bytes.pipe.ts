f06a45e66db1918cb6290ad2e4fb9701997ad89e
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-01-06 18:39:24 +0100

    feat: add pipe for formatting number of bytes to human readable format
    - i.e 1234 bytes will be transformed to '1.21 KB'

---
+++ projects/simbad-client/src/app/features/examples/simulation-pipeline/core/pipes/format-bytes.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatBytes } from '../functions/size-format-utils';
+
+@Pipe({
+    name: 'formatBytes'
+})
+export class FormatBytesPipe implements PipeTransform {
+
+    transform(value: any, args?: any): any {
+        return formatBytes(value);
+    }
+
+}
